40f362c047f4fb4c27f39c2928e39a6ead21c621
Robert Fiske <r.fiske@csuohio.edu>
2024-09-24 13:39:57 -0400

    add node

---
+++ f24_cis265/Node.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package f24_cis265;
+
+/**
+ *
+ * @author fiskeremote
+ */
+public class Node<T> {
+    T data;
+    Node<T> next;
+    public Node(T val)
+    {
+        data = val;
+    }
+}
